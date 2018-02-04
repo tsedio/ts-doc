3ba49b903727d9327553998fd8200033f2c41dc5
romain.lenzotti <rom.lenzotti@gmail.com>
2018-02-04 18:58:30 +0100

    fix: export

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-"use strict";
-const { buildApi } = require("./lib/tasks/build-api");
-/*
-const projectRoot = process.cwd();
-
-buildApi({
-  root: projectRoot,
-  apiDir: `${projectRoot}/docs/api`,
-  docsDir: `${projectRoot}/docs`,
-  srcDir: `${projectRoot}/src`,
-  libDir: `${projectRoot}/lib`
-});
-*/
-
-export {
+'use strict';
+const { buildApi } = require('./lib/tasks/build-api');
+modules.exports = {
   buildApi
 };
