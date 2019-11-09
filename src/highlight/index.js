414263de738d425a1418ba8af0919fded0d42ecf
romain.lenzotti <rom.lenzotti@gmail.com>
2019-11-09 10:25:12 +0100

    fix: Fix punctuation parsing

--- src/highlight/index.js
+++ src/highlight/index.js
@@ -1,78 +1,54 @@
-'use strict';
+'use strict'
 
-const { context } = require('../context');
-const KEYWORDS = /(\benum\b|\bstatic\b|\bclass\b|\binterface\b|\bprivate\b|\bpublic\b|\bconst\b|\blet\b|\bprotected\b|\bimplements\b|\bconstructor\b|\breadonly\b|\babstract\b|\bimport\b|\bexport\b|\bas\b|\bfrom\b|\bextends\b)/g;
-const TYPES = /(\bany\b|\bstring\b|\bboolean\b|\bnumber\b|\bDate\b|\bvoid\b)/g;
-const SEPARATOR = /(:|;|,|\(|\)|{|}|\[|\]| = )/g;
-/**
- *
- * @param match
- * @param $1
- * @param index
- * @param content
- * @returns {*}
- */
-const replacement = (match, $1, index, content) => {
-  if (context.symbols.has($1)) {
-    const symbol = context.symbols.get($1);
+const { context } = require('../context')
+const KEYWORDS = /(\benum\b|\bstatic\b|\bclass\b|\binterface\b|\bprivate\b|\bpublic\b|\bconst\b|\blet\b|\bprotected\b|\bimplements\b|\bconstructor\b|\breadonly\b|\babstract\b|\bimport\b|\bexport\b|\bas\b|\bfrom\b|\bextends\b)/g
+const TYPES = /(\bany\b|\bstring\b|\bboolean\b|\bnumber\b|\bDate\b|\bvoid\b)/g
+const SEPARATOR = /(:|;|,|\(|\)|{|}|\[|\]| = )/g
 
-    $1 = $1.replace(/</gi, '&lt;').replace(/>/gi, '&gt;');
+const replacement = (match, $1, index, content, ctx = context) => {
+  if (ctx.symbols.has($1)) {
+    const symbol = context.symbols.get($1)
 
-    return `<a href="${symbol.url}"><span class="token">${$1}</span></a>`;
+    $1 = $1.replace(/</gi, '&lt;').replace(/>/gi, '&gt;')
+
+    return `<a href="${symbol.url}"><span class="token">${$1}</span></a>`
   }
 
-  return match;
-};
-/**
- *
- * @param current
- * @returns {RegExp}
- */
-const symbolsMatch = (current) => {
+  return match
+}
 
-  const keys = [];
+const symbolsMatch = (current, ctx = context) => {
+  const keys = []
 
-  context.symbols.forEach(o => {
+  ctx.symbols.forEach(o => {
     if (current !== o.symbolName && o.symbolType !== 'function' && o.symbolName !== '') {
-      keys.push(o.symbolName.replace(/</gi, '&lt;').replace(/>/gi, '&gt;'));
+      keys.push(o.symbolName.replace(/</gi, '&lt;').replace(/>/gi, '&gt;'))
     }
-  });
+  })
 
-  return new RegExp('(\\b' + keys.join('\\b|\\b') + '\\b)', 'g');
-};
+  return new RegExp('(\\b' + keys.join('\\b|\\b') + '\\b)', 'g')
+}
 
-module.exports = {
-  /**
-   *
-   * @param content
-   * @param current
-   * @returns {*}
-   */
-  highlight(content, current) {
-
-    if (current === '') {
-      return content;
-    }
-    content = content
-      .replace(/</gi, '&lt;')
-      .replace(/>/gi, '&gt;')
-      .replace(KEYWORDS, '<span class="token keyword">$1<\/span>')
-      .replace(TYPES, '<span class="token keyword">$1<\/span>')
-      .replace(/(=|:) "(\w+)"/g, `$1 <span class="token string">"$2"<\/span>`)
-      .replace(/(\w+)\(/g, `<span class="token function">$1<\/span>(`)
-      .replace(SEPARATOR, '<span class="token punctuation">$1<\/span>');
-
-    content = content.replace(symbolsMatch(current), replacement);
-
-    return content;
-  },
-  /**
-   *
-   * @param content
-   * @param current
-   * @returns {string | void | * | {op, path, value, meta}}
-   */
-  bindSymbols(content, current) {
-    return content.replace(symbolsMatch(current), replacement);
+exports.highlight = (content, current, ctx = context) => {
+  if (current === '') {
+    return content
   }
-};
+  content = content
+    .replace(/</gi, '&lt;')
+    .replace(/>/gi, '&gt;')
+    .replace(KEYWORDS, '<span class="token keyword">$1<\/span>')
+    .replace(TYPES, '<span class="token keyword">$1<\/span>')
+    .replace(/(=|:) "(\w+)"/g, `$1 <span class="token string">"$2"<\/span>`)
+    .replace(/(\w+)\(/g, `<span class="token function">$1<\/span>(`)
+    .replace(SEPARATOR, '<span class="token punctuation">$1<\/span>')
+    .replace(/&gt<span class="token punctuation">;<\/span>/gi, '&gt;')
+    .replace(/&lt<span class="token punctuation">;<\/span>/gi, '&lt;')
+
+  content = content.replace(symbolsMatch(current, ctx), replacement)
+
+  return content
+}
+
+exports.bindSymbols = (content, current, ctx) =>
+  content.replace(symbolsMatch(current, ctx), (match, $1, index, content) =>
+    replacement(match, $1, index, content, ctx = context))
