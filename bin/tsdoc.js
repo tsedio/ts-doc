fd836229057bf9c859cac806dc00b5b4beca64d7
romain.lenzotti <rom.lenzotti@gmail.com>
2019-11-17 18:16:27 +0100

    fix: Add binary

--- bin/tsdoc.js
+++ bin/tsdoc.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const {buildApi} = require("../src/tasks/build-api");
 
-const { buildApi } = require('../src/tasks/build-api');
-
-buildApi({
+const file = `${process.cwd()}/tsdoc.config.js`;
+let config = {
   rootDir: process.cwd(),
-  packagesDir: 'packages/',
+  packagesDir: "packages/",
   scanPatterns: [
-    '<rootDir>/packages/**/lib/**/*.d.ts',
-    '!node_modules'
+    "<rootDir>/packages/**/lib/**/*.d.ts",
+    "!node_modules"
   ],
-  outputDir: '<rootDir>/docs/api',
-  baseUrl: '/api',
-  jsonOutputDir: '<rootDir>/docs/.vuepress/public',
+  outputDir: "<rootDir>/docs/api",
+  baseUrl: "/api",
+  jsonOutputDir: "<rootDir>/docs/.vuepress/public",
   modules: {}
-});
+};
+
+if (fs.existsSync(file)) {
+  config = require(file);
+}
+
+buildApi(config);
